ec38a47757c6bb5459a53719dd6ee09f7246e1ab
MisterMagik <denis.panek2004@op.pl>
2022-11-19 21:56:24 +0100

    add thread

--- BackupSaver/BackupSaver.cpp
+++ BackupSaver/BackupSaver.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <Windows.h>
 
+std::string locationFile;
+std::string locationFileText = "copy";
+std::string endText;
+int min = 0;
+void setUp() {
 
-
-int main()
-{
-    std::string locationFile;
-    std::string locationFileText = "copy";
-    std::string endText;
+  
     std::cout << "Paste your file location:\n";
     std::cin >> locationFile;
-    
+
     endText = locationFileText + " " + locationFile;
     std::cout << endText << std::endl;
 
@@ -18,9 +21,38 @@ int main()
 
     endText += " " + locationFile;
 
+    std::cout << "How many minutes: \n";
+    std::cin >> min;
 
-    std::system(endText.c_str());
-
+    
+    while (min < 0) {
+        std::cout << "How many minutes: \n";
+        std::cin >> min;
+    }
+    
+}
+void waitThread(int minutes) {
+   
+    std::this_thread::sleep_for(std::chrono::minutes(minutes));
+   
 }
 
+void backupFile() {
+    std::system(endText.c_str());
+}
 
+int main()
+{
+    setUp();
+    
+    for (;;) {
+        Sleep(2000);
+        std::thread th1(waitThread, min);
+        std::thread th2(backupFile);
+        th1.join();
+        th2.join();
+    }
+
+   
+
+}
