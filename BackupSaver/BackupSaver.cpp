102308134ac9d32d34ab1f7fcbd5da20c328e4d9
MisterMagik <denis.panek2004@op.pl>
2022-11-19 20:48:58 +0100

    bugfix

--- BackupSaver/BackupSaver.cpp
+++ BackupSaver/BackupSaver.cpp
@@ -1,6 +1,3 @@
-// BackupSaver.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 
@@ -14,9 +11,9 @@ int main()
     std::cin >> locationFile;
     
     endText = locationFileText + " " + locationFile;
-    std::cout << endText;
+    std::cout << endText << std::endl;
 
-    std::cout << "Paste your file destlation:\n";
+    std::cout << "Paste your file destination:\n";
     std::cin >> locationFile;
 
     endText += " " + locationFile;
@@ -26,13 +23,4 @@ int main()
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
